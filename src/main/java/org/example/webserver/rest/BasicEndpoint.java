345d9ad7de8dee3380753623b72d149a2f7458a3
Alexander Kombeiz <alexkombie@gmail.com>
2023-03-23 14:35:42 +0100

    WIP: init BasicEndpoint

---
+++ src/main/java/org/example/webserver/rest/BasicEndpoint.java
@@ -0,0 +1,24 @@
+package org.example.webserver.rest;
+
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+@Path("/the/best/rest")
+public class BasicEndpoint {
+    
+    private static final Logger LOGGER = Logger.getLogger(BasicEndpoint.class.getName());
+    
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getHelloWorld() {
+        LOGGER.log(Level.INFO, "I was called");
+        return "Hello World";
+    }
+}
---
+++ src/test/java/org/example/webserver/TestBasicEndpoint.java
@@ -0,0 +1,46 @@
+package org.example.webserver;
+
+
+import org.eclipse.jetty.server.Server;
+import org.example.webserver.rest.BasicEndpoint;
+import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class TestBasicEndpoint {
+    
+    private static final int PORT = 8080;
+    private static final String BASE_URI = "http://localhost:" + PORT + "/";
+    private static Server jettyServer;
+    
+    @BeforeClass
+    public static void startServer() throws Exception {
+        ResourceConfig config = new ResourceConfig(BasicEndpoint.class);
+        jettyServer = JettyHttpContainerFactory.createServer(URI.create(BASE_URI), config);
+        jettyServer.start();
+    }
+    
+    @AfterClass
+    public static void stopServer() throws Exception {
+        jettyServer.stop();
+    }
+    
+    @Test
+    public void testHelloWorld() throws IOException, InterruptedException {
+        HttpClient HTTPCLIENT = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(URI.create(BASE_URI)).GET().build();
+        HttpResponse<String> response = HTTPCLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+        assertEquals("Hello World", response.body());
+    }
+}
