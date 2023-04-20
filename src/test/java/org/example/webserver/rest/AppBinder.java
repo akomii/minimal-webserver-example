95db667fde6a42ec63fc4a9dbaf4441ebeae2aca
Alexander Kombeiz <alexkombie@gmail.com>
2023-04-20 15:04:42 +0200

    moved tests into subfolder

---
+++ src/test/java/org/example/webserver/rest/AppBinder.java
@@ -0,0 +1,16 @@
+package org.example.webserver.rest;
+
+import lombok.Setter;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+
+public class AppBinder extends AbstractBinder {
+    
+    @Setter
+    AppScopeComponent appComponent;
+    
+    @Override
+    protected void configure() {
+        bind(appComponent).to(InjectableComponentInterface.class);
+    }
+    
+}
--- src/test/java/org/example/webserver/rest/TestBasicEndpoint.java
+++ src/test/java/org/example/webserver/rest/TestBasicEndpoint.java
@@ -1,14 +1,12 @@
-package org.example.webserver;
+package org.example.webserver.rest;
 
 
 import org.eclipse.jetty.server.Server;
-import org.example.webserver.rest.AppScopeComponent;
-import org.example.webserver.rest.BasicEndpoint;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URI;
@@ -16,32 +14,32 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestBasicEndpoint {
     
     private static final String BASE_URI = "http://localhost:8080";
     private static Server jettyServer;
     
-    @BeforeClass
+    @BeforeAll
     public static void startServer() throws Exception {
         ResourceConfig config = new ResourceConfig(BasicEndpoint.class);
         AppScopeComponent inst = new AppScopeComponent();
         AppBinder binder = new AppBinder();
         binder.setAppComponent(inst);
         config.register(binder);
-        
         jettyServer = JettyHttpContainerFactory.createServer(URI.create(BASE_URI), config);
         jettyServer.start();
     }
     
-    @AfterClass
+    @AfterAll
     public static void stopServer() throws Exception {
-        jettyServer.stop();
+        if (jettyServer != null)
+            jettyServer.stop();
     }
     
     @Test
-    public void testHelloWorld() throws IOException, InterruptedException {
+    void testHelloWorld() throws IOException, InterruptedException {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(URI.create(BASE_URI + "/the/best/rest")).GET().build();
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
@@ -54,7 +52,7 @@ public class TestBasicEndpoint {
     }
     
     @Test
-    public void testBasicUserXML() throws IOException, InterruptedException {
+    void testBasicUserXML() throws IOException, InterruptedException {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(URI.create(BASE_URI + "/the/best/rest/user")).GET().header("Accept", "application/xml").build();
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
@@ -63,7 +61,7 @@ public class TestBasicEndpoint {
     }
     
     @Test
-    public void testBasicUserJSON() throws IOException, InterruptedException {
+    void testBasicUserJSON() throws IOException, InterruptedException {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(URI.create(BASE_URI + "/the/best/rest/user")).GET().header("Accept", "application/json").build();
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
